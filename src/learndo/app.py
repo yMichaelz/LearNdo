9193559f0b1565a2c2c9e269ad77e21e90115a27
Antony Michael Alves Chagas <antonyalvesz2004@gmail.com>
2025-03-24 11:33:25 -0300

    Testando Pydantic e Schemas

--- src/learndo/app.py
+++ src/learndo/app.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus as HTTPstatus  # type: ignore
+
 from fastapi import FastAPI  # type: ignore
+from fastapi.responses import HTMLResponse
+
+from learndo.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_model=Message, status_code=HTTPstatus.OK)
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.get("/html", response_class=HTMLResponse)
+def read_olamundo():
+    return "<h1>Hello World</h1>"
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,9 @@ def test_read_root_returns_ok():
     client.get("/")  # ACT (AÇÃO)
     response = client.get("/")  # ACT (AÇÃO)
     assert response.status_code == HTTPStatus.OK  # ASSERT (AFIRMAÇÃO)
+
+
+def test_read_root_returns_hello_world():
+    client = TestClient(app)  # ARRANGE (ORGANIZAÇÃO)
+    response = client.get("/")  # ACT (AÇÃO)
+    assert response.json() == {"message": "Hello World"}  # ASSERT (AFIRMAÇÃO)
